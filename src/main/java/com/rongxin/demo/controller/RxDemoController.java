933532de203892f7a9b4524626667d6ce184f9d3
gaoyukun@b2cf.cn <gaoyukun@b2cf.cn>
2022-08-10 16:11:23 +0800

    调整文件

--- src/main/java/com/rongxin/demo/controller/RxDemoController.java
+++ src/main/java/com/rongxin/demo/controller/RxDemoController.java
@@ -2,9 +2,6 @@ package com.rongxin.demo.controller;
 
 import java.util.List;
 
-import com.cacfintech.module.sms.aliyun.properties.ConstantsSms;
-import com.cacfintech.module.sms.aliyun.service.AliYunSmsService;
-import com.rongxin.common.utils.uuid.IdUtils;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
@@ -40,8 +37,6 @@ public class RxDemoController extends BaseController
 {
     @Autowired
     private IRxDemoService rxDemoService;
-    @Autowired
-    AliYunSmsService aliYunSmsService;
     /**
      * 查询示例功能列表
      */
@@ -124,18 +119,4 @@ public class RxDemoController extends BaseController
         return toAjax(rxDemoService.deleteRxDemoByIds(ids));
     }
 
-    @ApiOperation("发送短信")
-    @Log(title = "示例功能", businessType = BusinessType.DELETE)
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "smsCode", value = "短信编码", paramType = "query", required = true, dataType="String"),
-            @ApiImplicitParam(name = "mobile", value = "手机号码", paramType = "query", required = true, dataType="String"),
-            @ApiImplicitParam(name = "params", value = "参数", paramType = "query", required = true, dataType="String")
-    })
-    @PostMapping("send")
-    public AjaxResult send(String smsCode, String mobile, String params) throws Exception{
-        String randCode = aliYunSmsService.getRandCode(6);
-        String parm = "{\"code\":" + randCode + "}";
-        aliYunSmsService.sendSms("手机号", ConstantsSms.signName, ConstantsSms.templateCodeA, parm,true);
-        return AjaxResult.success();
-    }
 }
