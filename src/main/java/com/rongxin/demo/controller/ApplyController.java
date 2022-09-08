8ec21b20a69e57020b07a6101bb08684d8902a01
gaoyukun@b2cf.cn <gaoyukun@b2cf.cn>
2022-09-08 15:32:24 +0800

    调整通讯指向

--- src/main/java/com/rongxin/demo/controller/ApplyController.java
+++ src/main/java/com/rongxin/demo/controller/ApplyController.java
@@ -1,14 +1,18 @@
 package com.rongxin.demo.controller;
 
 import com.rongxin.common.core.domain.AjaxResult;
+import com.rongxin.common.utils.SecurityUtils;
 import com.rongxin.demo.service.ApplyService;
 import com.rongxin.framework.websocket.WebSocketServer;
+import com.rongxin.web.framework.web.service.impl.UserDetailsServiceImpl;
 import com.rongxin.wechatPay.bo.PayBo;
 import com.rongxin.wechatPay.errors.BusinessException;
 import com.rongxin.wechatPay.vo.PayVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +32,7 @@ import java.io.IOException;
 @Api(tags = "评估申请")
 @RequestMapping("/apply/test")
 public class ApplyController {
+    private static final Logger log = LoggerFactory.getLogger(ApplyController.class);
 
     @Resource
     ApplyService applyService;
@@ -52,7 +57,8 @@ public class ApplyController {
         String result = applyService.weChatPayCallBack(request);
         log.info("微信付款回调结束---------------------------------=｛｝", result);
         //前端发送消息
-        webSocketServer.sendInfo("有新消息!", "applyTest");
+        String userName = SecurityUtils.getUsername();
+        webSocketServer.sendInfo("有新消息!", userName);
         return AjaxResult.success(result);
     }
 
